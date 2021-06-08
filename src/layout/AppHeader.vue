c9a570e244a2016dc9c4ffb496c504a0fc5b5daf
denisearz <darzac@octopus.com.ar>
2021-06-08 19:05:36 -0300

    AdminInfo in progress

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -3,7 +3,7 @@
     <base-nav class="navbar-main" transparent type="" title="E-PROV" expand>
       <div class="card-body d-flex"></div>
 
-      <div class="register d-flex">
+      <div class="register d-flex" v-if="user_type == null || user_type == ''">
         <ul class="navbar-nav navbar-nav-hover align-items-lg-center active">
           <router-link class="nav-link" id="register" to="/register">
             Registrarse
@@ -23,6 +23,17 @@
           </button>
         </ul>
       </div>
+      <div v-else>
+        <input disabled="true" id="userType" type="text" readonly v-model="user_type" />
+        <button
+          type="button"
+          class="btn btn-sm active login"
+          id="register"
+          @click="logout"
+        >
+          Cerrar Sesion
+        </button>
+      </div>
     </base-nav>
   </header>
 </template>
@@ -39,8 +50,44 @@ export default {
     BaseDropdown,
     NavbarToggleButton,
   },
+  data() {
+    return {
+      user_type: "",
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.user_type = null;
+
+      this.$forceUpdate();
+    },
+  },
+
+  mounted() {
+    debugger;
+    let user_type = localStorage.getItem("currentUserType");
+    if (user_type !== null) {
+      this.user_type = user_type;
+    }
+  },
+  watch: {
+    $route(to, from) {
+      debugger;
+      let user_type = localStorage.getItem("currentUserType");
+      if (user_type !== null) {
+        this.user_type = user_type;
+      } else {
+        this.user_type = null;
+      }
+      this.$forceUpdate();
+    },
+  },
 };
 </script>
+
+
+
 <style>
 .header-global {
   background-color: #454546;
@@ -54,14 +101,18 @@ export default {
 #register {
   color: #fcfcf6;
   border-top: 1px solid #fdfd96;
-    border-bottom: 1px solid #fdfd96;
-    padding: 4px 0;
-    transition: 0.8s ease-out;
-    text-decoration: none;
+  border-bottom: 1px solid #fdfd96;
+  padding: 4px 0;
+  transition: 0.8s ease-out;
+  text-decoration: none;
 }
 #register:hover {
-    color: #fdfd96;
-    box-shadow: 2px 2px 22px 3px rgba(235, 214, 99, 0.315), 3px 2px 2px 3px rgba(255, 198, 10, 0.178);
-
+  color: #fdfd96;
+  box-shadow: 2px 2px 22px 3px rgba(235, 214, 99, 0.315),
+    3px 2px 2px 3px rgba(255, 198, 10, 0.178);
+}
+#userType {
+  color: #fcfcf6;
+  background-color: #454546;
 }
 </style>
